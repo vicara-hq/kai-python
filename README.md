e33688220c17410326b26c99959bcf2b2adffcbc
tarush <tarush@vicara.co>
2018-11-02 13:14:43 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+#Kai Python Package
+
+Sup?
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kai_python_package",
+    version="0.0.1",
+    author="Kai Software",
+    author_email="tarush@vicara.co",
+    description="A python package to use the sdk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
