b0347e452fefcaba9351b3597c6bec93b3a6a0d5
Tarush Nagpal <tarush.nagpal7@gmail.com>
2018-11-02 16:28:15 +0530

    changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="kai_python_package",
+    name="kaisdk",
     version="0.0.1",
     author="Kai Software",
     author_email="tarush@vicara.co",
