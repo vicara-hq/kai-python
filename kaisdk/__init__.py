4f17a80343630fd7689511eb2224880d50e73bf8
Tarush Nagpal <tarush.nagpal7@gmail.com>
2018-11-04 16:29:02 +0530

    added web socket

--- kaisdk/__init__.py
+++ kaisdk/__init__.py
@@ -1,2 +1,9 @@
-from . import pss
-from . import wss
+def is_authourized():
+    return(False)
+
+if(is_authourized):
+    from . import pss
+    from . import wss
+
+else:
+    print("No auth")
--- kaisdk/wss/__init__.py
+++ kaisdk/wss/__init__.py
@@ -1,5 +1,47 @@
-def get_data():
-    return("Not Implemented")
+from websocket import create_connection
+import websocket
+import json
 
-def set_capabilities(capabilities):
-    print(capabilities)
+host = "localhost"
+port = 2203
+buf = 1024
+addr = (host,port)
+
+websocket.enableTrace(True)
+ws = create_connection('ws://localhost:2203')
+
+print "Websocket active."
+print "address:\t"+host+":"+str(port)
+
+global_capabilities = [
+    "gestureData",
+    "fingerShortcutData",
+    "quaternionData",
+    "pyrData"
+]
+
+subscribed_capabilities = {
+    "type": "setCapabilities",
+    "gestureData": False,
+    "fingerShortcutData": False,
+    "quaternionData": False,
+    "pyrData": False
+}
+
+def set_capabilities( capabilities ):
+
+    if( type(capabilities) is list ):
+        for i in capabilities:
+            try:
+                subscribed_capabilities[str(i)] = True
+            except ValueError:
+                return("Wrong Format")
+
+            ws.send(json.dumps(subscribed_capabilities))
+
+    else:
+        return("Wrong format, must be a list")
+
+def receive_data():
+    result = ws.recv()
+    return('Result: {}'.format(result))
